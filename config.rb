2cae7b63b25b963f40a0f61c8fb847621fdf7a2d
randalmaile <randalmaile@gmail.com>
2014-03-25 16:33:17 -0700

    Heroku deployment test

--- config.rb
+++ config.rb
@@ -1,13 +1,13 @@
-require "lib/twitter_feed"
-require "lib/tumblr_feed"
+# require "lib/twitter_feed"
+# require "lib/tumblr_feed"
 
 ###
 # Global settings
 ###
 
 activate :directory_indexes
-activate :twitter_feed
-activate :tumblr_feed
+# activate :twitter_feed
+# activate :tumblr_feed
 ###
 # Compass
 ###
@@ -80,10 +80,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
