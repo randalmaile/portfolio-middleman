825bc4f6bbb53845c5b9c06b01e00eb3eb7cb47f
randalmaile <randalmaile@gmail.com>
2014-04-10 00:08:39 -0700

    againgit add .

--- config.rb
+++ config.rb
@@ -79,8 +79,27 @@ set :images_dir, 'images'
 configure :build do
   
   activate :imageoptim do |options|
-    options.pngout_options    =  false # Should disable pngout
-  end
+  # print out skipped images
+  options.verbose = false
+
+  # Setting these to true or nil will let options determine them (recommended)
+  options.nice = true
+  options.threads = true
+
+  # Image extensions to attempt to compress
+  options.image_extensions = %w(.png .jpg .gif .svg)
+
+  # compressor worker options, individual optimisers can be disabled by passing
+  # false instead of a hash
+  options.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
+  options.pngout_options    = false
+  options.optipng_options   = {:level => 6, :interlace => false}
+  options.advpng_options    = {:level => 4}
+  options.jpegoptim_options = {:strip => ['all'], :max_quality => 100}
+  options.jpegtran_options  = {:copy_chunks => false, :progressive => true, :jpegrescan => true}
+  options.gifsicle_options  = {:interlace => false}
+  options.svgo_options      = {}
+end
 
   # For example, change the Compass output style for deployment
   activate :minify_css
