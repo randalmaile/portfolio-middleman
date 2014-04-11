68d124d383bc6c4a868c9db47a4729112cf63cad
randalmaile <randalmaile@gmail.com>
2014-04-11 00:00:11 -0700

    removed sens data

--- lib/twitter_feed.rb
+++ lib/twitter_feed.rb
@@ -11,10 +11,10 @@ module TwitterFeed
     include Twitter::Autolink
     def tweet_news
       client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = "***REMOVED***"
-        config.consumer_secret     = "***REMOVED***"
-        config.access_token        = "***REMOVED***"
-        config.access_token_secret = "***REMOVED***"
+        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
+        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
       end
       client.user_timeline("randalmaile", count: "4")
     end
